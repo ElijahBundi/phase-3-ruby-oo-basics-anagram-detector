b4086f4ed1631a11179a8c27cd7840760ee54571
Elijah <bundielly@gmail.com>
2022-08-18 15:24:04 +0300

    commit tree 3ebfec2cea21d004075994c8baf9b14a53e191d0

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
 # Your code goes here!
+# class Anagram
+#     attr_accessor :word
+
+#     def initialize(word)
+#         @word = word
+#     end
+#     def match
+#         puts (%w[hello world zombies pants dipper])
+#     end
+
+# end
+class Pet
+    def initialize(name)
+      @name = name
+    end
+  
+    def name
+      @name
+    end
+  end
+  
+rabbit = Pet.new("Jabby")
+puts rabbit.name("Chokola")
